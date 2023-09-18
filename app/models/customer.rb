8feafd7bd59b1aaa21cd3f46c952c73b1a9f2518
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-18 06:07:04 +0000

    [Update] 顧客・注文model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :order, dependent: :destroy
+  has_many :cart_item, dependent: :destroy
+  has_many :address, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

e03905a95f3c9d992e23c35f9e4c6266b3b15c8b
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-18 06:16:14 +0000

    [Update] 顧客・注文model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
-  has_many :order, dependent: :destroy
-  has_many :cart_item, dependent: :destroy
-  has_many :address, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :addresses, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_detail
+  has_many :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
 end

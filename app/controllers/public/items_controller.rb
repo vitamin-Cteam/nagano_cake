bc5054f7de8da098b2c9c53add6e2e9c39d8bb27
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-09-18 12:03:26 +0000

    [Add]item show/index *in progress

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.where(is_on_sale: true).page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end

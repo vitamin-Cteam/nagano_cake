4e4ae754ec1cbde7088e35958a8b3ce3e6f1210c
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-09-19 00:18:18 +0000

    item

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,17 @@ class Item < ApplicationRecord
 
   has_one_attached :item_image
 
+    # 商品画像のサイズ調整
+  def get_item_image(width, height)
+    unless item_image.attached?
+      # 商品画像がない場合はimages/no-image.jpgを参照
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+
   def with_tax_price
     (price * 1.1).floor
   end

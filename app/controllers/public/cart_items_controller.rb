0543bfb9c203eed25ed252d5ad322a1ac72bd56c
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-09-19 11:54:52 +0000

    [Edit]cart item controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,32 +6,36 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    cart_item = current_customer.cart_items.find_by(:item_id)
-    cart_item.update(amount: cart_item_params.amount)
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    @cart_items = current_customer.cart_items.includes([:item])
+    render :index
   end
 
   def destroy
     cart_item = current_customer.cart_items.find(params[:id])
     cart_item.destroy
-  end
-
-  def destroy_all
-    current_customer.cart_items.destroy_all
+    @cart_items = current_customer.cart_items.includes([:item])
+    render :index
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    cart_item = current_customer.cart_items.find_by(item_id: params[:id])
-    if @cart_item.item.is_on_sale == true
-      if cart_item
-        amount = current_customer.cart_items.amount + cart_item_params.amount.to_i
-        @cart_item.update(amount: amount)
-      else
-        @cart_item.save
-      end
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path, notice: "商品を追加しました。"
+    elsif @cart_item.save
+      redirect_to cart_items_path, notice: "商品をカートに登録しました。"
+    else
+      redirect_to item_path(@cart_item.item), notice: "個数を選択してください"
     end
-    redirect_to cart_items_path
   end
 
   private

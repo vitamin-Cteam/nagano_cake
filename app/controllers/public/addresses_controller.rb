e9000a7419bf633292f8d17c744d3671b65b771f
yukayamaguchi18 <141097661+yukayamaguchi18@users.noreply.github.com>
2023-09-19 03:04:03 +0000

    [Edit]address controller/form

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,31 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.where(customer_id: current_customer.id)
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+  end
+
+  private
+  def address_params
+      params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end

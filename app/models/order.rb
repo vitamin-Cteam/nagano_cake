a0b23b277babdd2e5485901bdbf295ba6902f40a
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-21 01:13:16 +0000

    [Update] 注文機能・enum記述

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,5 +7,5 @@ class Order < ApplicationRecord
   validates :name, presence: true
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
+  enum status: { waiting: 0, confirm: 1, making: 2, preparing: 3, shipped: 4 }
 end
